6e8e6a7db19a3e2c8872a20b9ea848c183a353f8
samuelbagin1 <samuel.bagin1@gmail.com>
2025-06-25 21:18:08 +0200

    Initial Commit

---
+++ Quottary/QuottaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuottaryApp.swift
+//  Quottary
+//
+//  Created by Samuel Bagín on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuottaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuottaryTests/QuottaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuottaryTests.swift
+//  QuottaryTests
+//
+//  Created by Samuel Bagín on 25/06/2025.
+//
+
+import Testing
+@testable import Quottary
+
+struct QuottaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
