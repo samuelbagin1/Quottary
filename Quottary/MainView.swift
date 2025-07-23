f33d40f0672b9bdafb0e18cf80efc0d1ef7c07d0
samuelbagin1 <samuel.bagin1@gmail.com>
2025-07-23 15:46:58 +0200

    da

---
+++ Quottary/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Quottary
+//
+//  Created by Samuel Bagín on 21/07/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
